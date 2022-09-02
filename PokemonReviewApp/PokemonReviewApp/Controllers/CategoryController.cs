3623f51a286f80a258a53359baa8c206f298e2f2
weikhang95 <CWK95@hotmail.com>
2022-09-02 08:24:45 +0800

    Local

---
+++ PokemonReviewApp/PokemonReviewApp/Controllers/CategoryController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        // GET: api/values
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(pokemons);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
---
+++ PokemonReviewApp/PokemonReviewApp/Models/PokemonOwner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PokemonReviewApp.Models
+{
+    public class PokemonOwner
+    {
+        public int PokemonId { get; set; }
+        public int OwnerId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
+
